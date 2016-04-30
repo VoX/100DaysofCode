633f1e21e24d323fdad6efc43704c37da171b0aa
Ian Hills <ihills@protonmail.com>
2016-04-30 11:55:24 -0400

    crawllogger stub

---
+++ Web Crawler/CrawlLogger.cs
@@ -0,0 +1,15 @@
+namespace Web_Crawler
+{
+    internal class CrawlLogger
+    {
+        public CrawlLogger(string crawlerloggerLogTxt)
+        {
+            return;
+        }
+
+        public void AddLog(string target)
+        {
+            return;
+        }
+    }
+}
